c2210b2300742c1c41036255adfc4cff2e12f89f
lucas.nevespereira <lucas.nevespereira@eemi.com>
2020-06-15 12:19:49 +0200

    displaying random numbers in form

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-equation',
-  templateUrl: './equation.component.html',
-  styleUrls: ['./equation.component.css']
+  selector: "app-equation",
+  templateUrl: "./equation.component.html",
+  styleUrls: ["./equation.component.css"],
 })
 export class EquationComponent implements OnInit {
+  mathForm = new FormGroup({
+    a: new FormControl(this.randomNumber()),
+    b: new FormControl(this.randomNumber()),
+    anwser: new FormControl(""),
+  });
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  // Implementing getters
+  get a() {
+    return this.mathForm.value.a;
   }
 
+  get b() {
+    return this.mathForm.value.b;
+  }
+
+  ngOnInit(): void {}
+
+  // generate random number
+  randomNumber() {
+    return Math.floor(Math.random() * 10);
+  }
 }
